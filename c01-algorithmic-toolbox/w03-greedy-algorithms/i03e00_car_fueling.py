60b18f8d190feae34b6a9cc1e363f7e717cace23
Himel Das <himelhub@gmail.com>
2020-06-23 01:11:16 +0600

    refactor

---
+++ c01-algorithmic-toolbox/w03-greedy-algorithms/i03e00_car_fueling.py
@@ -0,0 +1,61 @@
+"""
+You are going from one city to another city situated at d distance.
+There are few fuel stations at the given distances. The tank capacity is given.
+Car starts with full tank. Find the minimum number of tank refill needed.
+
+Input: distance, tank_capacity, stops = 950, 400, [200, 375, 550, 750]
+Output: 2
+
+The distance between the cities is 950, the car can travel at most 400 miles
+on a full tank. It refills 2 times: at points 375 and 750. This is the minimum
+number of refills.
+"""
+
+import sys
+
+
+def compute_min_refills(distance, tank_capacity, stops):
+
+    # If distance between any 2 stops greater than tank, return -1.
+    # for(i=1; i<length; i++)
+    for i in range(1, len(stops)):
+        if tank_capacity < stops[i] - stops[i-1]:
+            return -1
+
+    # We are mutating the stops by adding 0 at the beginning
+    # and distance at the end which is our destination.
+    stops = [0] + stops + [distance]
+
+    count = 0
+    stops_count = len(stops)
+    remaining_fuel = tank_capacity
+
+    # for(i=0; i < n-1; i++)
+    for i in range(stops_count-1):
+        sub_path = stops[i+1] - stops[i]
+
+        if remaining_fuel < sub_path:
+            count += 1
+            remaining_fuel += tank_capacity
+
+        remaining_fuel -= sub_path
+
+    return count
+
+
+def test():
+    distance, tank_capacity, stops = 950, 400, [200, 375, 550, 750]
+    print(compute_min_refills(distance, tank_capacity, stops))
+
+    distance, tank_capacity, stops = 10, 3, [1, 2, 5, 9]
+    print(compute_min_refills(distance, tank_capacity, stops))
+
+
+if __name__ == '__main__':
+    submit = 0
+    if submit:
+        distance, tank_capacity, _, *stops = map(int, sys.stdin.read().split())
+        print(compute_min_refills(distance, tank_capacity, stops))
+
+    else:
+        test()
