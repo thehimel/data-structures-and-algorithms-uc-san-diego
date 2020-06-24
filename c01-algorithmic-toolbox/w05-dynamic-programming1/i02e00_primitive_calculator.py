bfa94721c05afe74bc991e72ad24569249f1e0dc
Himel Das <himelhub@gmail.com>
2020-06-24 16:40:42 +0600

    refactor

--- c01-algorithmic-toolbox/w05-dynamic-programming1/i02e00_primitive_calculator.py
+++ c01-algorithmic-toolbox/w05-dynamic-programming1/i02e00_primitive_calculator.py
@@ -1,21 +1,48 @@
-# Uses python3
 import sys
+
 
 def optimal_sequence(n):
     sequence = []
+
     while n >= 1:
         sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
+
+        if n % 3 == 0 or (n-1) % 3 == 0:
+            if n % 3 == 0:
+                n = n // 3
+            else:
+                n = n-1
+        elif n % 2 == 0 or (n-1) % 2 == 0:
+            if n % 2 == 0:
+                n = n // 2
+            else:
+                n = n-1
         else:
             n = n - 1
+
     return reversed(sequence)
 
-input = sys.stdin.read()
-n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
-for x in sequence:
-    print(x, end=' ')
+
+def test(n):
+    sequence = list(optimal_sequence(n))
+    print(len(sequence) - 1)
+    for x in sequence:
+        print(x, end=' ')
+    print('\n')
+
+
+if __name__ == '__main__':
+    submit = 0
+
+    if submit:
+        input = sys.stdin.read()
+        n = int(input)
+        sequence = list(optimal_sequence(n))
+        print(len(sequence) - 1)
+        for x in sequence:
+            print(x, end=' ')
+
+    else:
+        test(96234)
+        test(5)
+        test(1)
