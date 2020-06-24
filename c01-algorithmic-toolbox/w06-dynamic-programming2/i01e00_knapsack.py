c4ab1e28b3277cc15085f485ed2d44234e8a2f6c
Himel Das <himelhub@gmail.com>
2020-06-24 08:51:52 +0600

    refactor

---
+++ c01-algorithmic-toolbox/w06-dynamic-programming2/i01e00_knapsack.py
@@ -0,0 +1,14 @@
+# Uses python3
+import sys
+
+def optimal_weight(W, w):
+    result = 0
+    for x in w:
+        if result + x <= W:
+            result = result + x
+    return result
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    W, n, *w = list(map(int, input.split()))
+    print(optimal_weight(W, w))
