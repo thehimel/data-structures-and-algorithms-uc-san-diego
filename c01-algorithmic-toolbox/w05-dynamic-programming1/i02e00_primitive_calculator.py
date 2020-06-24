c5aa07c8900916d2253f07ed2a67a6e674d23b3f
Himel Das <himelhub@gmail.com>
2020-06-24 18:41:56 +0600

    refactor

--- c01-algorithmic-toolbox/w05-dynamic-programming1/i02e00_primitive_calculator.py
+++ c01-algorithmic-toolbox/w05-dynamic-programming1/i02e00_primitive_calculator.py
@@ -3,24 +3,48 @@ import sys
 
 def optimal_sequence(n):
     sequence = []
+    optimal_num = dynamic_optimal(n)
 
     while n >= 1:
-        sequence.append(n)
+        sequence = sequence + [n]
 
-        if n % 3 == 0 or (n-1) % 3 == 0:
-            if n % 3 == 0:
-                n = n // 3
-            else:
-                n = n-1
-        elif n % 2 == 0 or (n-1) % 2 == 0:
-            if n % 2 == 0:
-                n = n // 2
-            else:
-                n = n-1
+        if n % 3 == 0 and optimal_num[n//3 - 1] + 1 == optimal_num[n-1]:
+            n //= 3
+
+        elif n % 2 == 0 and optimal_num[n//2 - 1] + 1 == optimal_num[n-1]:
+            n //= 2
+
         else:
-            n = n - 1
+            n -= 1
 
     return reversed(sequence)
+
+
+def get_min(x, y, z):
+    if x == -1:
+        return get_min(z + 1, y, z)
+    if y == -1:
+        return get_min(x, z + 1, z)
+    else:
+        return min(x, y, z)
+
+
+def dynamic_optimal(n):
+    optimal_num = [0 for _ in range(n)]
+
+    for i in range(2, n+1):
+        first, second, third = -1, -1, -1
+
+        if i % 3 == 0:
+            third = optimal_num[i//3 - 1]
+
+        if i % 2 == 0:
+            second = optimal_num[i//2 - 1]
+
+        first = optimal_num[i - 1 - 1]
+        optimal_num[i - 1] = get_min(first, second, third) + 1
+
+    return optimal_num
 
 
 def test(n):
